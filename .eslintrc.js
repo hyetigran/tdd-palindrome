2f04de4b83c549a0cbda3595dff3a152e62efa50
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-03-18 15:11:37 +0400

    :truck: Move tests into their own folder

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['jest'],
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  env: {
+    node: true,
+  },
+};
